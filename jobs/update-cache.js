3ab2c2f211cc47a48493c6ed91e46f8ca36837af
Oskar Risberg <oskar.risberg@stendahls.se>
2021-03-25 15:53:33 +0100

    I'm not great with maps

--- jobs/update-cache.js
+++ jobs/update-cache.js
@@ -16,10 +16,15 @@ function replacer(key, value) {
 
 module.exports = async () => {
     console.log('Running cache update');
-    const itemData = await remoteData.get();
+    const itemMap = await remoteData.get();
+    const itemData = {};
+
+    for (const [key, value] of itemMap.entries()) {
+        itemData[key] = value;
+    }
 
     try {
-        const response = await cloudflare(`accounts/66766e138fce1ac1d2ef95953e037f4e/storage/kv/namespaces/f04e5b75ee894b3a90cec2b7cc351311/values/ITEM_CACHE?expiration_ttl=600`, 'PUT', JSON.stringify(itemData, replacer));
+        const response = await cloudflare(`accounts/66766e138fce1ac1d2ef95953e037f4e/storage/kv/namespaces/f04e5b75ee894b3a90cec2b7cc351311/values/ITEM_CACHE?expiration_ttl=600`, 'PUT', JSON.stringify(itemData));
         console.log(response);
     } catch (requestError){
         console.error(requestError);
