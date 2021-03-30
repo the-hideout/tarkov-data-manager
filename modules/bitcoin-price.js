d9bc61e967d4d97982140c718267254729a5c686
Oskar Risberg <oskar.risberg@stendahls.se>
2021-03-30 21:13:52 +0200

    reduce logging

--- modules/bitcoin-price.js
+++ modules/bitcoin-price.js
@@ -5,13 +5,13 @@ const BTC_PER_ITEM = 0.18;
 module.exports = () => {
     return new Promise((resolve, reject) => {
         https.get('https://api.coindesk.com/v1/bpi/currentprice/rub.json', (res) => {
-            console.log('statusCode:', res.statusCode);
-            console.log('headers:', res.headers);
+            // console.log('statusCode:', res.statusCode);
+            // console.log('headers:', res.headers);
 
             res.on('data', (d) => {
                 const parsedData = JSON.parse(d);
 
-                console.log(parsedData);
+                // console.log(parsedData);
                 resolve(Math.floor(parsedData.bpi.RUB.rate_float * BTC_PER_ITEM));
             });
         }).on('error', (e) => {
