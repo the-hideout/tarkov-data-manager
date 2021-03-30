33851ffe1d83f05006f214e5ba363abfa9c93073
Oskar Risberg <oskar.risberg@stendahls.se>
2021-03-30 22:15:55 +0200

    don't expire the cache

--- jobs/update-cache.js
+++ jobs/update-cache.js
@@ -24,7 +24,7 @@ module.exports = async () => {
     }
 
     try {
-        const response = await cloudflare(`accounts/66766e138fce1ac1d2ef95953e037f4e/storage/kv/namespaces/f04e5b75ee894b3a90cec2b7cc351311/values/ITEM_CACHE?expiration_ttl=600`, 'PUT', JSON.stringify(itemData));
+        const response = await cloudflare(`accounts/66766e138fce1ac1d2ef95953e037f4e/storage/kv/namespaces/f04e5b75ee894b3a90cec2b7cc351311/values/ITEM_CACHE`, 'PUT', JSON.stringify(itemData));
         console.log(response);
     } catch (requestError){
         console.error(requestError);
