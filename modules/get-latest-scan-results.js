10953e472ae99670a2b331305044cdc14a133f1a
Oskar Risberg <oskar.risberg@stendahls.se>
2021-05-19 09:39:32 +0200

    add some timing

--- modules/get-latest-scan-results.js
+++ modules/get-latest-scan-results.js
@@ -11,10 +11,13 @@ connection.connect();
 
 module.exports = async () => {
     return new Promise((resolve, reject) => {
+        console.time('latest-scan-query');
         connection.query('select max(timestamp) as timestamp, source from price_data group by source order by `timestamp` desc', (error, results) => {
             if(error){
                 return reject(error);
             }
+
+            console.timeEnd('latest-scan-query');
 
             return resolve(results);
         });
