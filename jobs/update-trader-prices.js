4637a49a2d0c4121ca9450eb47a082d8f1f1cca8
Oskar Risberg <oskar.risberg@stendahls.se>
2021-07-08 09:17:56 +0200

    add job to update trader prices

---
+++ jobs/update-trader-prices.js
@@ -0,0 +1,75 @@
+require('dotenv').config();
+
+const fs = require('fs');
+const path = require('path');
+
+const cloudflare = require('../modules/cloudflare');
+const doQuery = require('../modules/do-query');
+
+module.exports = async () => {
+    console.log('Running trader inventory update');
+
+    const traderItems = await doQuery(`SELECT
+        *
+    FROM
+        trader_items;`);
+
+    const traderPriceData = await doQuery(`SELECT
+        *
+    FROM
+        trader_price_data;`);
+
+    const latestTraderPrices = {};
+
+    for(const traderPrice of traderPriceData){
+        if(!latestTraderPrices[traderPrice.trade_id]){
+            latestTraderPrices[traderPrice.trade_id] = {
+                price: traderPrice.price,
+                timestamp: traderPrice.timestamp,
+            };
+
+            continue;
+        }
+
+        if(latestTraderPrices[traderPrice.trade_id].timestamp.getTime() > traderPrice.timestamp.getTime()){
+            continue;
+        }
+
+        latestTraderPrices[traderPrice.trade_id] = {
+            price: traderPrice.price,
+            timestamp: traderPrice.timestamp,
+        };
+    }
+
+    const outputData = {};
+
+    for(const traderItem of traderItems){
+        if(!latestTraderPrices[traderItem.id]){
+            continue;
+        }
+
+        if(!outputData[traderItem.item_id]){
+            outputData[traderItem.item_id] = [];
+        }
+
+        outputData[traderItem.item_id].push({
+            id: traderItem.item_id,
+            source: traderItem.trader_name,
+            min_level: traderItem.min_level,
+            price: latestTraderPrices[traderItem.id].price,
+            updated: latestTraderPrices[traderItem.id].timestamp,
+            quest_unlock: Boolean(traderItem.quest_unlock_id),
+            quest_unlock_id: Number(traderItem.quest_unlock_id),
+            currency: traderItem.currency,
+        });
+    }
+
+    fs.writeFileSync(path.join(__dirname, 'dumps', 'trader-inventory.json'), JSON.stringify(outputData, null, 4));
+
+    try {
+        const response = await cloudflare(`accounts/66766e138fce1ac1d2ef95953e037f4e/storage/kv/namespaces/f04e5b75ee894b3a90cec2b7cc351311/values/TRADER_ITEMS`, 'PUT', JSON.stringify(outputData));
+        console.log(response);
+    } catch (requestError){
+        console.error(requestError);
+    }
+};
---
+++ modules/do-query.js
@@ -0,0 +1,37 @@
+const mysql = require('mysql');
+
+module.exports = async function doQuery(query) {
+    const connection = mysql.createConnection({
+        host     : 'tarkov-tools-master-1.cluster-c1vhfeufwkpn.eu-west-1.rds.amazonaws.com',
+        user     : 'desktop1',
+        password : process.env.MYSQL_PASSWORD,
+        database : 'tarkov_tools',
+    });
+
+    connection.connect();
+
+    let responseData;
+    const promise = new Promise((resolve, reject) => {
+        connection.query(query
+            , async (error, results) => {
+                if (error) {
+                    reject(error)
+                }
+
+                resolve(results);
+            }
+        );
+    });
+
+    try {
+        responseData = await promise;
+    } catch (upsertError){
+        console.error(upsertError);
+
+        throw upsertError;
+    }
+
+    connection.end();
+
+    return responseData;
+}';'
