34ded2648c6780a284818d382f24633d0ffaafc7
Oskar Risberg <oskar.risberg@stendahls.se>
2021-07-08 09:20:22 +0200

    minor refactor

--- jobs/update-cache.js
+++ jobs/update-cache.js
@@ -1,57 +1,10 @@
 require('dotenv').config();
 
-const mysql = require('mysql');
 const roundTo = require('round-to');
 
 const cloudflare = require('../modules/cloudflare');
 const remoteData = require('../modules/remote-data');
-
-function replacer(key, value) {
-    if(value instanceof Map) {
-        return {
-            dataType: 'Map',
-            value: Array.from(value.entries()), // or with spread: value: [...value]
-        };
-    } else {
-        return value;
-    }
-}
-
-async function doQuery(query) {
-    const connection = mysql.createConnection({
-        host     : 'tarkov-tools-master-1.cluster-c1vhfeufwkpn.eu-west-1.rds.amazonaws.com',
-        user     : 'desktop1',
-        password : process.env.MYSQL_PASSWORD,
-        database : 'tarkov_tools',
-    });
-
-    connection.connect();
-
-    let responseData;
-    const promise = new Promise((resolve, reject) => {
-        connection.query(query
-            , async (error, results) => {
-                if (error) {
-                    reject(error)
-                }
-
-                resolve(results);
-            }
-        );
-    });
-
-    try {
-        responseData = await promise;
-    } catch (upsertError){
-        console.error(upsertError);
-
-        throw upsertError;
-    }
-
-    connection.end();
-
-    return responseData;
-}
+const doQuery = require('../modules/do-query');
 
 module.exports = async () => {
     console.log('Running cache update');
