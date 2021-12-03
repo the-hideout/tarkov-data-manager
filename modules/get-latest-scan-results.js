76433f1f4092eba2e0745a639297a987ae27100d
Razzmatazz <razz@badkarma.xyz>
2021-12-03 16:56:22 -0600

    increase time span for listed scanners

--- modules/get-latest-scan-results.js
+++ modules/get-latest-scan-results.js
@@ -3,7 +3,7 @@ const connection = require('./db-connection');
 module.exports = async () => {
     return new Promise((resolve, reject) => {
         console.time('latest-scan-query');
-        connection.query('select max(timestamp) as timestamp, source from price_data where timestamp > DATE_SUB(CURDATE(), INTERVAL 1 HOUR) group by source order by `timestamp` desc', (error, results) => {
+        connection.query('select max(timestamp) as timestamp, source from price_data where timestamp > DATE_SUB(CURDATE(), INTERVAL 7 DAY) group by source order by `timestamp` desc', (error, results) => {
             if(error){
                 return reject(error);
             }
