3fee5af6dbf24d992abc58b2ec55e1f8d0a01b65
Razzmatazz <razz@badkarma.xyz>
2021-12-14 17:04:07 -0600

    move materialize js to footer

--- index.js
+++ index.js
@@ -330,6 +330,7 @@ const getHeader = (req) => {
 
 const getFooter = (req) => {
     return `
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
         </body>
     </html>`;
 };
