54a1a502a0e2877042901599c6a1123535102ec7
Oskar Risberg <oskar.risberg@stendahls.se>
2021-12-14 16:42:38 +0100

    Increase rendering performance of checkboxes

--- index.js
+++ index.js
@@ -285,7 +285,6 @@ const getHeader = (req) => {
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
 
             <!-- Compiled and minified JavaScript -->
-            <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
             <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
             <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
             <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
