a9d164beda09e9d922dfaaea24bc820bf205df4d
Oskar Risberg <oskar.risberg@stendahls.se>
2021-12-18 23:50:48 +0100

    store cloudflare data locally for debugging

--- modules/cloudflare.js
+++ modules/cloudflare.js
@@ -1,14 +1,21 @@
+const fs = require('fs');
+const path = require('path');
+
 const got = require('got');
 
 const BASE_URL = 'https://api.cloudflare.com/client/v4/';
 
-const doRequest = async (path, method = 'GET', value) => {
+const doRequest = async (cloudflarePath, method = 'GET', value) => {
     const requestOptions = {
         method: method,
         headers: {
             'authorization': `Bearer ${process.env.CLOUDFLARE_TOKEN}`,
         },
     };
+
+    const objectData = JSON.parse(value);
+
+    fs.writeFileSync(path.join(__dirname, '..', 'dumps', `${cloudflarePath.split("/").pop().toLowerCase()}.json`), JSON.stringify(objectData, null, 4));
 
     if(value){
         requestOptions.body = value;
@@ -17,7 +24,7 @@ const doRequest = async (path, method = 'GET', value) => {
     let response;
 
     try {
-        response = await got(`${BASE_URL}${path}`, requestOptions);
+        response = await got(`${BASE_URL}${cloudflarePath}`, requestOptions);
     } catch (requestError){
         console.log(requestError);
     }
