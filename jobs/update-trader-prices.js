565be1aa8485e0f90037b11d72e189024f44a940
Razzmatazz <--system>
2022-03-03 11:36:04 -0600

    USD/EUR prices reflect latest exchange rates

--- jobs/update-trader-prices.js
+++ jobs/update-trader-prices.js
@@ -5,7 +5,7 @@ const cloudflare = require('../modules/cloudflare');
 const doQuery = require('../modules/do-query');
 
 module.exports = async () => {
-    const scanOffsetTimestamp = await doQuery(`SELECT
+    const junkboxLastScan = await doQuery(`SELECT
         *
     FROM
         trader_price_data
@@ -16,7 +16,70 @@ module.exports = async () => {
         desc
     LIMIT 1`);
 
-    scanOffsetTimestamp[0].timestamp.setHours(scanOffsetTimestamp[0].timestamp.getHours() - 6);
+    const scanOffsetTimestamp = new Date(junkboxLastScan[0].timestamp).setHours(junkboxLastScan[0].timestamp.getHours() - 6);
+
+    const currencyISO = {
+        '5696686a4bdc2da3298b456a': 'USD',
+        '569668774bdc2da2298b4568': 'EUR'
+    }
+    const currenciesNow = {
+        'RUB': 1
+    };
+    const currenciesThen = {
+        'RUB': 1
+    };
+    const currenciesLastScan = await doQuery(`
+        SELECT
+            item_id, trader_name, currency, min_level, quest_unlock_id,
+            price, trader_items.timestamp as offer_timestamp, trader_price_data.timestamp as price_timestamp
+        FROM
+            trader_items
+        INNER JOIN 
+            trader_price_data
+        ON
+            trader_items.id=trader_price_data.trade_id
+        WHERE
+            item_id in ('5696686a4bdc2da3298b456a', '569668774bdc2da2298b4568') AND
+            trader_price_data.timestamp=(
+                SELECT 
+                    timestamp 
+                FROM 
+                    trader_price_data
+                WHERE 
+                    trade_id=trader_items.id
+                ORDER BY timestamp DESC
+                LIMIT 1
+            );
+    `);
+    for (const curr of currenciesLastScan) {
+        currenciesNow[currencyISO[curr.item_id]] = curr.price;
+    }
+    const currenciesHistoricScan = await doQuery(`
+        SELECT
+            item_id, trader_name, currency, min_level, quest_unlock_id,
+            price, trader_items.timestamp as offer_timestamp, trader_price_data.timestamp as price_timestamp
+        FROM
+            trader_items
+        INNER JOIN 
+            trader_price_data
+        ON
+            trader_items.id=trader_price_data.trade_id
+        WHERE
+            item_id in ('5696686a4bdc2da3298b456a', '569668774bdc2da2298b4568') AND
+            trader_price_data.timestamp=(
+                SELECT 
+                    tpd.timestamp 
+                FROM 
+                    trader_price_data tpd
+                WHERE 
+                    tpd.trade_id=trader_items.id
+                ORDER BY abs(UNIX_TIMESTAMP(tpd.timestamp) - ?)
+                LIMIT 1
+            );
+    `, junkboxLastScan[0].timestamp.getTime()/1000);
+    for (const curr of currenciesHistoricScan) {
+        currenciesThen[currencyISO[curr.item_id]] = curr.price;
+    }
 
     const traderItems = await doQuery(`SELECT
         *
@@ -63,11 +126,12 @@ module.exports = async () => {
             outputData[traderItem.item_id] = [];
         }
 
+        const rublesCost = currenciesThen[traderItem.currency]*latestTraderPrices[traderItem.id].price;
         outputData[traderItem.item_id].push({
             id: traderItem.item_id,
             source: traderItem.trader_name,
             min_level: traderItem.min_level,
-            price: latestTraderPrices[traderItem.id].price,
+            price: Math.ceil(rublesCost / currenciesNow[traderItem.currency]),
             updated: latestTraderPrices[traderItem.id].timestamp,
             quest_unlock: Boolean(traderItem.quest_unlock_id),
             quest_unlock_id: traderItem.quest_unlock_id,
