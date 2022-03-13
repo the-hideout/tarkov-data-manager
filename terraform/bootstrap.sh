41112f48b1a228a883f75d82a704ea91b536c9af
Grant Birkinbine <grantbirki@github.com>
2022-03-13 14:49:36 -0700

    improve bootstrap

--- terraform/bootstrap.sh
+++ terraform/bootstrap.sh
@@ -12,6 +12,7 @@ sudo chmod +x /usr/local/bin/docker-compose
 
 # clone the tarkov-data-manager repo
 git clone https://github.com/the-hideout/tarkov-data-manager.git /home/tdm/tarkov-data-manager
+sudo chown -R tdm:tdm tarkov-data-manager/
 
 # firewall connections for ssh
 sudo ufw allow 22
@@ -19,6 +20,8 @@ sudo ufw allow 22
 # firewall connections for web traffic
 sudo ufw allow 80
 sudo ufw allow 443
+sudo ufw allow http
+sudo ufw allow https
 
 # enable the firewall
 sudo ufw --force enable
@@ -29,5 +32,3 @@ sudo ln -s /snap/bin/certbot /usr/bin/certbot
 
 # finish off with one more update
 sudo apt-get update && sudo apt-get upgrade -y
-
-# 0 7 * * SUN ~/borscht/script/cert-update
