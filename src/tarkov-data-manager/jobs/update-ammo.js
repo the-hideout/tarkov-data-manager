bb8d41a1be39643be3e1fc4e39d4215c558c3556
Razzmatazz <razz@badkarma.xyz>
2022-04-15 08:28:48 -0500

    don't grab disabled ammo types

--- src/tarkov-data-manager/jobs/update-ammo.js
+++ src/tarkov-data-manager/jobs/update-ammo.js
@@ -6,6 +6,7 @@ const cloudflare = require('../modules/cloudflare');
 const JobLogger = require('../modules/job-logger');
 const {alert} = require('../modules/webhook');
 const tarkovChanges = require('../modules/tarkov-changes');
+const { query, jobComplete } = require('../modules/db-connection');
 
 module.exports = async function() {
     const logger = new JobLogger('update-ammo');
@@ -58,11 +59,20 @@ module.exports = async function() {
             if (typeof caliberCounts[ammo._props.Caliber] === 'undefined') caliberCounts[ammo._props.Caliber] = 0;
             caliberCounts[ammo._props.Caliber]++;
         }
+        const ammoIds = ammunition.data.map(ammo => `'${ammo.id}'`);
+        const results = await query(`
+            SELECT item_data.id FROM item_data
+            LEFT JOIN types ON
+                types.item_id = item_data.id
+            WHERE NOT EXISTS (SELECT type FROM types WHERE item_data.id = types.item_id AND type = 'disabled')
+            AND item_data.id IN (${ammoIds.join(', ')})
+        `);
+        const validIds = results.map(result => result.id);
+        ammunition.data = ammunition.data.filter(ammo => validIds.includes(ammo.id));
         logger.log(`Processed ${ammunition.data.length} ammunition types`);
         for (const cal in caliberCounts) {
             logger.log(`${cal}: ${caliberCounts[cal]}`);
         }
-    
         fs.writeFileSync(path.join(__dirname, '..', 'dumps', 'ammo.json'), JSON.stringify(ammunition, null, 4));
 
         const response = await cloudflare(`/values/AMMO_DATA`, 'PUT', JSON.stringify(ammunition)).catch(error => {
@@ -88,4 +98,5 @@ module.exports = async function() {
         });
     }
     logger.end();
+    jobComplete();
 };
