145e685bcb71f9c020e00eccd72661fb5bea86a5
Razzmatazz <razz@badkarma.xyz>
2022-07-16 16:48:10 -0500

    add queue times to maps

--- src/tarkov-data-manager/jobs/update-queue-times.js
+++ src/tarkov-data-manager/jobs/update-queue-times.js
@@ -1,36 +1,19 @@
 const moment = require('moment');
+const got = require('got');
+
 const cloudflare = require('../modules/cloudflare');
 const { query, jobComplete } = require('../modules/db-connection');
 const JobLogger = require('../modules/job-logger');
 const { alert } = require('../modules/webhook');
-const got = require('got');
-
-// Helper function to query the database and return records for a given timestamp
-const queueQuery = async (time) => {
-    const result = await query(`
-            SELECT
-                *
-            FROM
-                queue_data
-            WHERE
-                timestamp > ?;
-        `, [time]);
-
-    return result;
-}
+const jobOutput = require('../modules/job-output');
 
 // Helper function which creates a fresh map array
-const setupMapArray = async (allMaps) => {
+const setupMapArray = (allMaps) => {
     // Create a dictionary of all in-game maps with their time set to zero and totalEntries set to 0
     var maps = {};
     for (const map of allMaps) {
-        // if the map contains the word 'night', skip it
-        if (map.name.toLowerCase().includes('night')) {
-            continue;
-        }
-
         // add the map to the maps object and set the time to 0 - make sure it is lowercase
-        maps[map.name.toLowerCase()] = { time: 0, totalEntries: 0 };
+        maps[map.name.toLowerCase()] = {map_id: map.id, time: 0, totalEntries: 0 };
     }
 
     return maps;
@@ -41,52 +24,79 @@ module.exports = async () => {
     logger = new JobLogger('update-queue-times');
     try {
         // Get times to filter by
-        var timestamps = [];
+        const timestamps = [];
         timestamps.push(moment().subtract(1, 'hours').format('YYYY-MM-DD HH:mm:ss'));
         timestamps.push(moment().subtract(6, 'hours').format('YYYY-MM-DD HH:mm:ss'));
         timestamps.push(moment().subtract(12, 'hours').format('YYYY-MM-DD HH:mm:ss'));
         timestamps.push(moment().subtract(1, 'days').format('YYYY-MM-DD HH:mm:ss'));
         timestamps.push(moment().subtract(7, 'days').format('YYYY-MM-DD HH:mm:ss'));
 
-        // Fetch all current maps from the API
-        const allMaps = await got('https://api.tarkov.dev/graphql?query={maps{name}}', {
-            responseType: 'json',
-        });
+        // Fetch all current maps
+        const allMaps = await jobOutput('update-maps', './dumps/map_data.json', logger);
 
-        // Loop through the timestamps array and make a SQL query with each one
-        var queueTimes = {};
+        const queueTimes = {};
         for (const timestamp of timestamps) {
-            var maps = await setupMapArray(allMaps.body.data.maps)
-
-            // Query the database for records with the given timestamp
-            const result = await queueQuery(timestamp);
-
-            // Loop through the results and add the queue times to the maps object
-            for (const row of result) {
-                const map = row.map;
-                const time = row.time;
-                // const type = row.type; // this value is not currently uses - for future use with scav / pmc raid types
-
-                // Add queue time to the respective map and increment the totalEntries
-                maps[map].time += time;
-                maps[map].totalEntries++;
-            }
-
-            // Calculate the average queue time for each map using the time value and the totalEntries value
-            // Append the average queue time which is matched to the 'map' key to the queueData array
-            var queueData = [];
-            for (const map in maps) {
-                queueData.push({ map: map, time: maps[map].time / maps[map].totalEntries });
-            }
-
-            // Add the results to the queueTimes object
-            queueTimes[timestamp] = queueData;
+            queueTimes[timestamp] = setupMapArray(allMaps);
         }
 
-        console.log(JSON.stringify(queueTimes));
+        // Query all queue times from the longest possible time window
+        const queueTimeResults = await query(`
+            SELECT
+                *
+            FROM
+                queue_data
+            WHERE
+                timestamp > ?;
+        `, [timestamps[timestamps.length-1]]);
+        // Loop through the results and add the queue times to the maps object
+        for (const row of queueTimeResults) {
+            const map = row.map;
+            const time = row.time;
+            // const type = row.type; // this value is not currently uses - for future use with scav / pmc raid types
+            // Loop through the timestamps array and see if the row makes the cut
+            for (const timestamp of timestamps) {
+                if (timestamp> row.time) continue;
+                // Add queue time to the respective map and increment the totalEntries
+                queueTimes[timestamp][map].time += time;
+                queueTimes[timestamp][map].totalEntries++;
+            }
+        }
 
-        // Somewhere around here, we need to PUT the queueTimes object to the cloudflare KV store
-        // await cloudflare.set(`queue_times_${map}_${type}`, queueTimes);
+        // Calculate the average queue time for each map using the time value and the totalEntries value
+        // Append the average queue time which is matched to the 'map' key to the queueData array
+        const queueTimeData = [];
+        for (const timestamp in queueTimes) {
+            const maps = queueTimes[timestamp];
+            for (const map in maps) {
+                queueTimeData.push({
+                    cutoff: moment(timestamp).fromNow(),
+                    map_id: maps[map].map_id, 
+                    time: maps[map].time / maps[map].totalEntries 
+                });
+            }
+        }
+
+        console.log(JSON.stringify(queueTimeData));
+
+        // PUT the queueTimes data to the cloudflare KV store
+        const queueData = {
+            updated: new Date(),
+            data: queueTimeData,
+        };
+        let response = await cloudflare.put('queue_data', JSON.stringify(queueData)).catch(error => {
+            logger.error(error);
+            return {success: false, errors: [], messages: []};
+        });
+        if (response.success) {
+            logger.success('Successful Cloudflare put of queue_data');
+        } else {
+            for (const error of response.errors) {
+                logger.error(error);
+            }
+            for (const warning of response.messages) {
+                logger.error(warning);
+            }
+        }
 
     } catch (error) {
         logger.error(error);
@@ -94,8 +104,8 @@ module.exports = async () => {
             title: `Error running ${logger.jobName} job`,
             message: error.toString()
         });
-        logger.end();
-        jobComplete();
-        logger = false;
     }
+    await jobComplete();
+    logger.end();
+    logger = false;
 };
