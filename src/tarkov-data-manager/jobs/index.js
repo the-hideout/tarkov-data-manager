42d14a27c69c244a5c16054ae13679a8144a58f1
Razzmatazz <razz@badkarma.xyz>
2022-08-31 22:35:01 -0500

    add cron schedule info, change schedule

--- src/tarkov-data-manager/jobs/index.js
+++ src/tarkov-data-manager/jobs/index.js
@@ -33,7 +33,7 @@ const defaultJobs = {
     'update-crafts': '1-59/5 * * * *',
     'update-existing-bases': '4-59/5 * * * *',
     'game-data': '*/10 * * * *',
-    'update-historical-prices': '5-59/15 * * * *',
+    'update-historical-prices': '30 */2 * * *',
     'update-trader-prices': '25 9,21 * * *',
     'clear-checkouts': '5,35 * * * *',
     'verify-wiki': '5 9 * * *',
