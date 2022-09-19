da95c31294624096f04d590cccb0f39442d680d1
Razzmatazz <razz@badkarma.xyz>
2022-09-19 07:09:00 -0500

    change schedule of check-image-links job

--- src/tarkov-data-manager/jobs/index.js
+++ src/tarkov-data-manager/jobs/index.js
@@ -31,7 +31,7 @@ const defaultJobs = {
     'update-traders': '*/5 * * * *',
     'update-barters': '*/5 * * * *',
     'update-crafts': '1-59/5 * * * *',
-    'check-image-links': '4-59/5 * * * *',
+    'check-image-links': '37 */6 * * *',
     'game-data': '*/10 * * * *',
     'update-historical-prices': '30 * * * *',
     'update-trader-prices': '25 9,21 * * *',
