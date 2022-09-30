719dff62773294007efbea3e74216500bf53f02c
Razzmatazz <razz@badkarma.xyz>
2022-09-30 11:52:11 -0500

    only download ru from tarkov bot

--- src/tarkov-data-manager/jobs/update-lang.js
+++ src/tarkov-data-manager/jobs/update-lang.js
@@ -1,4 +1,5 @@
 const tarkovBot = require('../modules/tarkov-bot');
+const tarkovChanges = require('../modules/tarkov-changes');
 const JobLogger = require('../modules/job-logger');
 const {alert} = require('../modules/webhook');
 
@@ -7,7 +8,8 @@ module.exports = async (externalLogger) => {
     try {
         logger.log('Downloading language data from Tarkov-Bot...');
         logger.time('tb-download');
-        await tarkovBot.locales(true, logger);
+        //await tarkovBot.locales(true, logger);
+        await tarkovBot.dictionary(true, 'locale_ru.json', 'ru')
         logger.timeEnd('tb-download');
         logger.success('Successfully downloaded data from Tarkov-Bot');
     } catch (error) {
