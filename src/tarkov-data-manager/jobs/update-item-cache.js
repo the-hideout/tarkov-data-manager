cb564bd3a8c27e8ffda91ce51b8a70a07f0bc2f9
Razzmatazz <razz@badkarma.xyz>
2022-10-10 15:33:45 -0500

    remove debugging output

--- src/tarkov-data-manager/jobs/update-item-cache.js
+++ src/tarkov-data-manager/jobs/update-item-cache.js
@@ -421,9 +421,7 @@ module.exports = async () => {
             } else if (credits[key]) {
                 itemData[key].basePrice = credits[key];
             } 
-            if (itemData[key].properties && !itemData[key].properties.propertiesType) {
-                console.log('lacks propertiesType', itemData[key])
-            }
+            
             itemData[key].wikiLink = itemData[key].wiki_link;
             itemData[key].link = `https://tarkov.dev/item/${itemData[key].normalizedName}`;
 
