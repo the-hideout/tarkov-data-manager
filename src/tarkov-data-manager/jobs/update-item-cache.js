cc1d42c136ca9f5dc9aa7da54e2c4997a0720191
Razzmatazz <razz@badkarma.xyz>
2023-01-14 14:10:25 -0600

    handbook pls

--- src/tarkov-data-manager/jobs/update-item-cache.js
+++ src/tarkov-data-manager/jobs/update-item-cache.js
@@ -509,6 +509,9 @@ module.exports = async () => {
         
         // populate child ids for handbook categories
         Object.values(handbookCategories).forEach(cat => {
+            if (handbookCategories[cat.parent_id]?.child_ids.includes(cat.id)) {
+                return;
+            }
             handbookCategories[cat.parent_id]?.child_ids.push(cat.id);
         });
 
