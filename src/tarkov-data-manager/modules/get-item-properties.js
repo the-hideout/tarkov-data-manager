69f60d9360fd35e38f21c6b3beb4b1258e606ed8
Razzmatazz <razz@badkarma.xyz>
2023-02-24 09:17:59 -0600

    add backpack stats

--- src/tarkov-data-manager/modules/get-item-properties.js
+++ src/tarkov-data-manager/modules/get-item-properties.js
@@ -231,6 +231,9 @@ const getItemProperties = async (item) => {
     } else if (item._parent === '5448e53e4bdc2d60728b4567') {
         properties = {
             propertiesType: 'ItemPropertiesBackpack',
+            speedPenalty: parseInt(item._props.speedPenaltyPercent) / 100,
+            turnPenalty: parseInt(item._props.mousePenalty) / 100,
+            ergoPenalty: parseInt(item._props.weaponErgonomicPenalty),
             ...getGrids(item)
         };
     } else if (item._parent === '543be6564bdc2df4348b4568') {
