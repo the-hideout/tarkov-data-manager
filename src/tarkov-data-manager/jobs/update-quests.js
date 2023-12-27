0fd9f29e94a422f2acaf148cfbfea2a74147a539
Razzmatazz <razz@badkarma.xyz>
2023-12-27 10:34:04 -0600

    fix update quests

--- src/tarkov-data-manager/jobs/update-quests.js
+++ src/tarkov-data-manager/jobs/update-quests.js
@@ -71,6 +71,10 @@ class UpdateQuestsJob extends DataJob {
                 this.logger.warn(`Skipping event quest ${this.locales.en[`${questId} name`]} ${questId}`);
                 continue;
             }
+            if (!this.locales.en[`${questId} name`]) {
+                this.logger.warn(`Skipping quest ${this.rawQuestData[questId].QuestName} ${questId} - localization not found`);
+                continue;
+            }
             quests.Task.push(this.formatRawQuest(this.rawQuestData[questId]));
         }
         
