3bc56e98952a9b56c9229ee1063c4602a8945948
Razzmatazz <razz@badkarma.xyz>
2024-08-12 15:58:34 -0500

    Create separate data for PVE hideout and crafts

--- src/tarkov-data-manager/jobs/update-crafts.mjs
+++ src/tarkov-data-manager/jobs/update-crafts.mjs
@@ -26,200 +26,207 @@ class UpdateCraftsJob extends DataJob {
         const areas = await this.jobManager.jobOutput('update-hideout', this);
         const tasks = await this.jobManager.jobOutput('update-quests', this);
         const presets = await this.jobManager.jobOutput('update-presets', this);
-        const crafts = {
-            Craft: [],
-        };
-        const stations = {};
-        const inactiveStations = {};
-        this.logger.log('Processing crafts...');
-        for (const id in json) {
-            const craft = json[id];
-            const station = areas.find(area => area.areaType === craft.areaType);
-            if (!station) {
-                if (!inactiveStations[en[`hideout_area_${craft.areaType}_name`]]) {
-                    inactiveStations[en[`hideout_area_${craft.areaType}_name`]] = 0;
-                }
-                inactiveStations[en[`hideout_area_${craft.areaType}_name`]]++;
-                continue;
-            }
-            let endProduct = processedItems.get(craft.endProduct);
-            if (!endProduct) {
-                this.logger.warn(`${id}: No end product item with id ${craft.endProduct} found in items`);
-                continue;
-            }
-            if (endProduct.types.includes('disabled')) {
-                this.logger.warn(`${id}: End product ${endProduct.name} ${craft.endProduct} is disabled`);
-                continue;
-            }
-            if (endProduct.types.includes('quest')) {
-                this.logger.warn(`${id}: End product ${endProduct.name} ${craft.endProduct} is a quest item`);
-                continue;
-            }
-            if (endProduct.types.includes('gun')) {
-                const preset = Object.values(presets).find(p => p.baseId === endProduct.id && p.default);
-                if (preset) {
-                    endProduct = processedItems.get(preset.id);
-                }
-            }
-            if (craft.locked && !craft.requirements.some(req => req.type === 'QuestComplete')) {
-                this.logger.warn(`${id}: Craft for ${endProduct.name} is locked`);
-                continue;
-            }
-            if (skipCrafts.includes(id)) {
-                //this.logger.warn(`${id}: Craft for ${endProduct.name} is skipped`);
-                continue;
-            }
-            if (!stations[en[station.name]]) {
-                stations[en[station.name]] = 0;
-            }
-            const craftData = {
-                id: id,
-                requiredItems: [],
-                requiredQuestItems: [],
-                rewardItems: [{
-                    name: endProduct.name,
-                    item: endProduct.id,
-                    count: craft.count,
-                    attributes: []
-                }],
-                station: station.id,
-                station_id: station.id,
-                sourceName: en[station.name],
-                duration: craft.productionTime,
-                requirements: [],
-                gameEditions: [],
+        for (const gameMode of this.gameModes) {
+            const json = await tarkovData.crafts({gameMode: gameMode.name});
+            this.kvData[gameMode.name] = {
+                Craft: [],
             };
-            let level = false;
-            let skip = false;
-            for (const index in craft.requirements) {
-                const req = craft.requirements[index];
-                if (req.type === 'Area') {
-                    //craftData.station = craftData.station + ' level '+req.requiredLevel;
-                    craftData.requirements.push({
-                        type: 'stationLevel',
-                        value: req.requiredLevel
-                    });
-                    craftData.level = req.requiredLevel;
-                    level = req.requiredLevel;
-                } else if (req.type === 'Resource') {
-                    if (!items[req.templateId]) {
-                        this.logger.warn(`${id}: Resource ${en[`${req.templateId} Name`]} ${req.templateId} not found in items.json`);
-                        continue;
+            const stations = {};
+            const inactiveStations = {};
+            this.logger.log(`Processing ${gameMode.name} crafts...`);
+            for (const id in json) {
+                const craft = json[id];
+                const station = areas.find(area => area.areaType === craft.areaType);
+                if (!station) {
+                    if (!inactiveStations[en[`hideout_area_${craft.areaType}_name`]]) {
+                        inactiveStations[en[`hideout_area_${craft.areaType}_name`]] = 0;
                     }
-                    const resourceItem = processedItems.get(req.templateId);
-                    if (!resourceItem) {
-                        this.logger.warn(`${id}: Resource ${en[`${req.templateId} Name`]} ${req.templateId} not found in items`);
-                        continue;
+                    inactiveStations[en[`hideout_area_${craft.areaType}_name`]]++;
+                    continue;
+                }
+                let endProduct = processedItems.get(craft.endProduct);
+                if (!endProduct) {
+                    this.logger.warn(`${id}: No end product item with id ${craft.endProduct} found in items`);
+                    continue;
+                }
+                if (endProduct.types.includes('disabled')) {
+                    this.logger.warn(`${id}: End product ${endProduct.name} ${craft.endProduct} is disabled`);
+                    continue;
+                }
+                if (endProduct.types.includes('quest')) {
+                    this.logger.warn(`${id}: End product ${endProduct.name} ${craft.endProduct} is a quest item`);
+                    continue;
+                }
+                if (endProduct.types.includes('gun')) {
+                    const preset = Object.values(presets).find(p => p.baseId === endProduct.id && p.default);
+                    if (preset) {
+                        endProduct = processedItems.get(preset.id);
                     }
-                    if (resourceItem.types.includes('disabled')) {
-                        this.logger.warn(`${id}: Resource ${resourceItem.name} ${req.templateId} is disabled`);
-                        continue;
-                    }
-                    if (resourceItem.types.includes('quest')) {
-                        this.logger.warn(`${id}: Resource ${resourceItem.name} ${req.templateId} is a quest item`);
-                        continue;
-                    }
-                    craftData.requiredItems.push({
-                        name: processedItems.get(req.templateId).name,
-                        item: req.templateId,
-                        count: req.resource / items[req.templateId]._props.Resource,
+                }
+                if (craft.locked && !craft.requirements.some(req => req.type === 'QuestComplete')) {
+                    this.logger.warn(`${id}: Craft for ${endProduct.name} is locked`);
+                    continue;
+                }
+                if (skipCrafts.includes(id)) {
+                    //this.logger.warn(`${id}: Craft for ${endProduct.name} is skipped`);
+                    continue;
+                }
+                if (!stations[en[station.name]]) {
+                    stations[en[station.name]] = 0;
+                }
+                const craftData = {
+                    id: id,
+                    requiredItems: [],
+                    requiredQuestItems: [],
+                    rewardItems: [{
+                        name: endProduct.name,
+                        item: endProduct.id,
+                        count: craft.count,
                         attributes: []
-                    });
-                } else if (req.type === 'Item') {
-                    const ingredient = processedItems.get(req.templateId);
-                    if (!ingredient) {
-                        this.logger.warn(`${id}: Ingredient item ${en[`${req.templateId} Name`]} ${req.templateId} found in items`);
-                        continue;
-                    }
-                    if (ingredient.types.includes('disabled')) {
-                        this.logger.warn(`${id}: Ingredient ${ingredient.name} ${ingredient.id} is disabled`);
-                        continue;
-                    }
-                    if (ingredient.types.includes('quest')) {
-                        this.logger.warn(`${id}: Ingredient ${ingredient.name} ${ingredient.id} is a quest item`);
-                        continue;
-                    }
-                    const reqData = {
-                        name: processedItems.get(req.templateId).name,
-                        item: req.templateId,
-                        count: req.count,
-                        attributes: []
-                    };
-                    if (req.isFunctional) {
-                        reqData.attributes.push({
-                            type: 'functional',
-                            value: String(req.isFunctional)
-                        })
-                    }
-                    craftData.requiredItems.push(reqData);
-                } else if (req.type == 'Tool') {
-                    const toolItem = processedItems.get(req.templateId);
-                    if (!toolItem) {
-                        this.logger.warn(`${id}: Unknown tool ${en[`${req.templateId} Name`]} ${req.templateId}`);
-                        if (items[req.templateId] && items[req.templateId]._props.QuestItem) {
-                            skip = true;
+                    }],
+                    station: station.id,
+                    station_id: station.id,
+                    sourceName: en[station.name],
+                    duration: craft.productionTime,
+                    requirements: [],
+                    gameEditions: [],
+                };
+                let level = false;
+                let skip = false;
+                for (const index in craft.requirements) {
+                    const req = craft.requirements[index];
+                    if (req.type === 'Area') {
+                        //craftData.station = craftData.station + ' level '+req.requiredLevel;
+                        craftData.requirements.push({
+                            type: 'stationLevel',
+                            value: req.requiredLevel
+                        });
+                        craftData.level = req.requiredLevel;
+                        level = req.requiredLevel;
+                    } else if (req.type === 'Resource') {
+                        if (!items[req.templateId]) {
+                            this.logger.warn(`${id}: Resource ${en[`${req.templateId} Name`]} ${req.templateId} not found in items.json`);
+                            continue;
                         }
-                        continue;
-                    }
-                    if (toolItem.types.includes('quest')) {
-                        craftData.requiredQuestItems.push({
+                        const resourceItem = processedItems.get(req.templateId);
+                        if (!resourceItem) {
+                            this.logger.warn(`${id}: Resource ${en[`${req.templateId} Name`]} ${req.templateId} not found in items`);
+                            continue;
+                        }
+                        if (resourceItem.types.includes('disabled')) {
+                            this.logger.warn(`${id}: Resource ${resourceItem.name} ${req.templateId} is disabled`);
+                            continue;
+                        }
+                        if (resourceItem.types.includes('quest')) {
+                            this.logger.warn(`${id}: Resource ${resourceItem.name} ${req.templateId} is a quest item`);
+                            continue;
+                        }
+                        craftData.requiredItems.push({
+                            name: processedItems.get(req.templateId).name,
+                            item: req.templateId,
+                            count: req.resource / items[req.templateId]._props.Resource,
+                            attributes: []
+                        });
+                    } else if (req.type === 'Item') {
+                        const ingredient = processedItems.get(req.templateId);
+                        if (!ingredient) {
+                            this.logger.warn(`${id}: Ingredient item ${en[`${req.templateId} Name`]} ${req.templateId} found in items`);
+                            continue;
+                        }
+                        if (ingredient.types.includes('disabled')) {
+                            this.logger.warn(`${id}: Ingredient ${ingredient.name} ${ingredient.id} is disabled`);
+                            continue;
+                        }
+                        if (ingredient.types.includes('quest')) {
+                            this.logger.warn(`${id}: Ingredient ${ingredient.name} ${ingredient.id} is a quest item`);
+                            continue;
+                        }
+                        const reqData = {
+                            name: processedItems.get(req.templateId).name,
+                            item: req.templateId,
+                            count: req.count,
+                            attributes: []
+                        };
+                        if (req.isFunctional) {
+                            reqData.attributes.push({
+                                type: 'functional',
+                                value: String(req.isFunctional)
+                            })
+                        }
+                        craftData.requiredItems.push(reqData);
+                    } else if (req.type == 'Tool') {
+                        const toolItem = processedItems.get(req.templateId);
+                        if (!toolItem) {
+                            this.logger.warn(`${id}: Unknown tool ${en[`${req.templateId} Name`]} ${req.templateId}`);
+                            if (items[req.templateId] && items[req.templateId]._props.QuestItem) {
+                                skip = true;
+                            }
+                            continue;
+                        }
+                        if (toolItem.types.includes('quest')) {
+                            craftData.requiredQuestItems.push({
+                                name: toolItem.name,
+                                item: req.templateId,
+                            });
+                            continue;
+                        }
+                        craftData.requiredItems.push({
                             name: toolItem.name,
                             item: req.templateId,
+                            count: 1,
+                            attributes: [{
+                                type: 'tool',
+                                value: String(true)
+                            }]
                         });
-                        continue;
+                    } else if (req.type === 'QuestComplete') {
+                        const task = tasks.find(q => q.id === req.questId);
+                        if (!task) {
+                            this.logger.warn(`${id}: Unknown quest unlock ${en[`${req.questId} name`]} ${req.questId}`);
+                            continue;
+                        }
+                        craftData.requirements.push({
+                            type: 'questCompleted',
+                            value: task.tarkovDataId,
+                            stringValue: task.id,
+                        });
+                        craftData.taskUnlock = task.id;
+                    } else if (req.type === 'GameVersion') {
+                        craftData.gameEditions = req.gameVersions;
+                    } else {
+                        this.logger.warn(`${id}: Unknown craft requirement type ${req.type}`);
                     }
-                    craftData.requiredItems.push({
-                        name: toolItem.name,
-                        item: req.templateId,
-                        count: 1,
-                        attributes: [{
-                            type: 'tool',
-                            value: String(true)
-                        }]
-                    });
-                } else if (req.type === 'QuestComplete') {
-                    const task = tasks.find(q => q.id === req.questId);
-                    if (!task) {
-                        this.logger.warn(`${id}: Unknown quest unlock ${en[`${req.questId} name`]} ${req.questId}`);
-                        continue;
-                    }
-                    craftData.requirements.push({
-                        type: 'questCompleted',
-                        value: task.tarkovDataId,
-                        stringValue: task.id,
-                    });
-                    craftData.taskUnlock = task.id;
-                } else if (req.type === 'GameVersion') {
-                    craftData.gameEditions = req.gameVersions;
-                } else {
-                    this.logger.warn(`${id}: Unknown craft requirement type ${req.type}`);
                 }
+                if (skip) {
+                    continue;
+                }
+                if (!level) {
+                    //craftData.station = craftData.station + ' level 1';
+                    craftData.requirements.push({
+                        type: 'stationLevel',
+                        value: 1
+                    });
+                    craftData.level = 1;
+                }
+                craftData.source = `${en[station.name]} level ${craftData.level}`;
+                this.kvData[gameMode.name].Craft.push(craftData);
+                stations[en[station.name]]++;
             }
-            if (skip) {
-                continue;
+            for (const stationName in stations) {
+                this.logger.log(`✔️ ${stationName}: ${stations[stationName]}`);
             }
-            if (!level) {
-                //craftData.station = craftData.station + ' level 1';
-                craftData.requirements.push({
-                    type: 'stationLevel',
-                    value: 1
-                });
-                craftData.level = 1;
+            for (const stationName in inactiveStations) {
+                this.logger.log(`❌ ${stationName}: ${inactiveStations[stationName]}`);
             }
-            craftData.source = `${en[station.name]} level ${craftData.level}`;
-            crafts.Craft.push(craftData);
-            stations[en[station.name]]++;
+            this.logger.log(`Processed ${this.kvData[gameMode.name].Craft.length} active ${gameMode.name} crafts`);
+    
+            let kvName = this.kvName;
+            if (gameMode.name !== 'regular') {
+                kvName += `_${gameMode.name}`;
+            }
+            await this.cloudflarePut(this.kvData[gameMode.name], kvName);
         }
-        for (const stationName in stations) {
-            this.logger.log(`✔️ ${stationName}: ${stations[stationName]}`);
-        }
-        for (const stationName in inactiveStations) {
-            this.logger.log(`❌ ${stationName}: ${inactiveStations[stationName]}`);
-        }
-        this.logger.log(`Processed ${crafts.Craft.length} active crafts`);
-
-        await this.cloudflarePut(crafts);
-        return crafts;
+        return this.kvData;
     }
 }
 
