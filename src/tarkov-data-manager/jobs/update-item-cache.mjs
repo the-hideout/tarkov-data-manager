81d441bdd16b02e9ce7b986d1a08931e4e0164f0
Razzmatazz <razz@badkarma.xyz>
2025-04-18 10:01:12 -0500

    simplify level group calculation

--- src/tarkov-data-manager/jobs/update-item-cache.mjs
+++ src/tarkov-data-manager/jobs/update-item-cache.mjs
@@ -1,6 +1,3 @@
-import fs from 'node:fs/promises';
-import path from 'node:path';
-
 import DataJob from '../modules/data-job.mjs';
 import dataMaps from '../modules/data-map.js';
 import remoteData from '../modules/remote-data.mjs';
@@ -29,7 +26,6 @@ class UpdateItemCacheJob extends DataJob {
             this.itemMap,
             this.handbook,
             this.traders,
-            this.playerLevelGroups,
         ] = await Promise.all([
             tarkovData.items(), 
             tarkovData.credits({gameMode: 'regular'}),
@@ -40,7 +36,6 @@ class UpdateItemCacheJob extends DataJob {
             }),
             tarkovData.handbook(),
             tarkovData.traders({gameMode: 'regular'}),
-            fs.readFile(path.join(import.meta.dirname, '..', 'data', 'player-level-groups.json')).then(json => JSON.parse(json)),
         ]);
         this.logger.log('Getting presets...');
         this.presets = await this.jobManager.jobOutput('update-presets', this, 'regular', true);
@@ -435,16 +430,8 @@ class UpdateItemCacheJob extends DataJob {
                 return allArmor;
             }, {}),
             PlayerLevel: this.globals.config.exp.level.exp_table.map((level, index) => {
-                let levelGroup = '1';
                 const playerLevel = index + 1;
-                for (const levelGroupNum in this.playerLevelGroups) {
-                    const levelBounds = this.playerLevelGroups[levelGroupNum];
-                    if (playerLevel < levelBounds[0] || playerLevel > levelBounds[1]) {
-                        continue;
-                    }
-                    levelGroup = levelGroupNum;
-                    break;
-                }
+                const levelGroup = Math.trunc(playerLevel / 5) + 1;
                 return {
                     level: playerLevel,
                     exp: level.exp,
