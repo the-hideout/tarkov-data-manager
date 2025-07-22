18ecc3159ed4e4c85c1c5c639c4a1a2cc861515e
Razzmatazz <razz@badkarma.xyz>
2025-07-22 08:42:12 -0500

    faster data update

--- src/tarkov-data-manager/jobs/update-main-data.mjs
+++ src/tarkov-data-manager/jobs/update-main-data.mjs
@@ -22,22 +22,25 @@ class UpdateMainDataJob extends DataJob {
 
         const returnValue = {};
         this.logger.time('data-download');
+        const reqs = [];
         for (const gameMode of gameModes) {
             this.logger.log(`Downloading ${gameMode.name} data...`);
             
-            returnValue[gameMode.name] = await tarkovData.downloadAll({returnErrors: true, gameMode: gameMode.name}).then(results => {
+            reqs.push(tarkovData.downloadAll({returnErrors: true, gameMode: gameMode.name}).then(results => {
                 const errors = results.errors;
                 results.errors = undefined;
                 if (Object.keys(results).length > 0) {
-                    this.logger.success(`Downloaded: ${Object.keys(results).filter(key => key !== 'errors').join(', ')}`);
+                    this.logger.success(`Downloaded ${gameMode.name}: ${Object.keys(results).filter(key => key !== 'errors').join(', ')}`);
                 }
                 if (errors) {
                     this.logger.warn(`Error downloading ${gameMode.name} data: ${Object.keys(errors).map(file => `${file}: ${errors[file].message}`).join(', ')}`);
                     Object.keys(errors).forEach(file => this.addJobSummary(`${file}: ${errors[file].message}`, `Error(s) updating ${gameMode.name} data`));
                 }
+                returnValue[gameMode.name] = results;
                 return results;
-            });
+            }));
         }
+        await Promise.all(reqs);
         this.logger.timeEnd('data-download');
         return returnValue;
     }
