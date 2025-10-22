d73cd2bbaa387c6be6b092bea8d7fb78d003caf1
Razzmatazz <razz@badkarma.xyz>
2025-10-22 10:47:12 -0500

    exclude no-translation type

--- src/tarkov-data-manager/jobs/update-item-cache.mjs
+++ src/tarkov-data-manager/jobs/update-item-cache.mjs
@@ -14,6 +14,7 @@ import { getLocalBucketContents, uploadAnyImage } from '../modules/upload-s3.mjs
 const excludeTypes = [
     'onlyFlea',
     'noHandbook',
+    'noTranslation',
 ];
 
 class UpdateItemCacheJob extends DataJob {
