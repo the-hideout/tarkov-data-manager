257464bcda1c297c54d96faa56c5cf46512bb895
Razzmatazz <razz@badkarma.xyz>
2025-11-08 21:07:07 -0600

    skip invalid location index

--- src/tarkov-data-manager/modules/tarkov-spt.mjs
+++ src/tarkov-data-manager/modules/tarkov-spt.mjs
@@ -319,7 +319,7 @@ const tarkovSpt = {
             try {
                 oldLocationIndex = JSON.parse(fs.readFileSync(cachePath(`spt_location_${id}_index.json`)));
             } catch (error) {
-                if (error.code !== 'ENOENT') {
+                if (error.code !== 'ENOENT' && !error.message.includes('Unexpected end of JSON input')) {
                     return Promise.reject(error);
                 }
             }
