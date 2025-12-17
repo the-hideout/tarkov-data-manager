cc1eec0511e2b1cf758cd717eb5c63210c65b704
Razzmatazz <razz@badkarma.xyz>
2025-12-17 13:35:43 -0600

    disable don't remove

--- src/tarkov-data-manager/jobs/update-presets.mjs
+++ src/tarkov-data-manager/jobs/update-presets.mjs
@@ -196,9 +196,10 @@ class UpdatePresetsJob extends DataJob {
                 continue;
             }
             const p = this.presetsData[id];
-            if (!p) {
-                this.logger.warn(`Preset ${item.name} ${id} is no longer valid; removing`);
-                queries.push(presetsHelper.deletePreset(id));
+            if (!p && !item.types.includes('disabled')) {
+                this.logger.warn(`Preset ${item.name} ${id} is no longer valid; disabling`);
+                //queries.push(presetsHelper.deletePreset(id));
+                queries.push(remoteData.addType(id, 'disabled'));
                 continue;
             }
             if (p.armorOnly) {
