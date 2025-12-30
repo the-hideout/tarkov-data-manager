188f756272e454490d667ddc3265645348701de7
Razzmatazz <razz@badkarma.xyz>
2025-12-30 04:55:21 -0600

    change items locale kv name

--- src/tarkov-data-manager/jobs/update-item-cache.mjs
+++ src/tarkov-data-manager/jobs/update-item-cache.mjs
@@ -438,7 +438,7 @@ class UpdateItemCacheJob extends DataJob {
         this.logger.log('Uploading items data to cloudflare...');
         await this.fillTranslations(this.itemsLocale.locale);
         await this.cloudflarePut();
-        await this.cloudflarePut(this.itemsLocale, 'items_locale_data');
+        await this.cloudflarePut(this.itemsLocale, `${this.kvName}_locale`);
 
         //this.logger.log('Uploading handbook data to cloudflare...');
         //handbookData.locale = await this.translationHelper.fillTranslations();
@@ -516,7 +516,7 @@ class UpdateItemCacheJob extends DataJob {
             modeData.SpecialItems = this.getSpecialItems();
             this.logger.log(`Uploading ${gameMode.name} items data to cloudflare...`);
             await this.cloudflarePut(modeData, `${this.kvName}_${gameMode.name}`);
-            await this.cloudflarePut(this.itemsLocale, `items_locale_data_${gameMode.name}`);
+            await this.cloudflarePut(this.itemsLocale, `${this.kvName}_locale_${gameMode.name}`);
 
             //this.logger.log(`Uploading ${gameMode.name} handbook data to cloudflare...`);
             //handbookData.locale = await this.translationHelper.fillTranslations();
