074eded6e11bcddc4c07ea71bf46d4085f470f0c
Razzmatazz <razz@badkarma.xyz>
2026-05-06 06:56:16 -0500

    actual fix

--- src/tarkov-data-manager/modules/remote-data.mjs
+++ src/tarkov-data-manager/modules/remote-data.mjs
@@ -524,7 +524,7 @@ const methods = {
             myData.set(values.id, {
                 ...currentItemData,
                 ...values,
-                types: currentItemData.types,
+                types: currentItemData.types ?? [],
                 updated: currentItemData?.updated ?? new Date(),
             });
             emitter.emit('itemAdded', myData.get(values.id));
